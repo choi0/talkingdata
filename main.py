615ae55ff9772d4e8bbc860e049362d7a1854478
Choi, Danny P <danny.choi@optum.com>
2018-03-29 17:10:35 -0400

    add forloop to read csv

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 """
 Module Docstring
 """
-
+import csv
+import sys
 
 __author__ = "Danny Choi"
 __version__ = "0.1.0"
@@ -12,6 +13,15 @@ __license__ = "MIT"
 def main():
     """ Main entry point of the app """
     print("hello world")
+    f = open("train_sample.csv", 'rt')
+    reader = csv.reader(f, delimiter=',')
+    for x in range(0, 3):
+        currLine = next(reader)
+        print(currLine)
+        for index in range(len(currLine)):
+            print(currLine[index])
+
+    f.close()
 
 
 if __name__ == "__main__":
