fa34cca883ff597ef77dbd9f393452ff435f12b6
Choi, Danny P <danny.choi@optum.com>
2018-03-29 16:47:54 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+import openpyxl
+
+__author__ = "Danny Choi"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()git remote add origin https://github.com/choi0/talkingdata.git
