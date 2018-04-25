aa2633d0bcad70c537b4c1395fda9a64f265dac6
choi0 <choidanny94@gmail.com>
2018-04-24 20:42:46 -0400

    add working code, gets 53.3

--- final.py
+++ final.py
@@ -7,6 +7,7 @@ import csv
 import pandas as pd
 import numpy as np
 import pydot
+import tensorflow as tf
 import graphviz
 from keras.models import Sequential
 #from keras.utils import plot_model
@@ -22,6 +23,23 @@ __license__ = "MIT"
 
 def mean_pred(y_true, y_pred):
     return K.mean(y_pred)
+
+def auc_roc(y_true, y_pred):
+    # any tensorflow metric
+    value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
+
+    # find all variables created for this metric
+    metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
+
+    # Add metric variables to GLOBAL_VARIABLES collection.
+    # They will be initialized for new session.
+    for v in metric_vars:
+        tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
+
+    # force to update metric values
+    with tf.control_dependencies([update_op]):
+        value = tf.identity(value)
+        return value
 
 def preprocessTraining(df):
     #preprocess data
@@ -93,11 +111,12 @@ def main():
 
     # create model
     model = Sequential()
-    model.add(Dense(5, input_dim=9, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(6, input_dim=9, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(3, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal', activation='tanh'))
     # Compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', mean_pred])
-    model.fit(x_train, y_train, epochs=64, batch_size=128)
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=8, batch_size=128)
     results = model.predict(x_train)
     results = np.where(results > 0.5, 1, 0)
     #plot_model(model, to_file='model.png')
